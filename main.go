975c1548aac923ad839560b06a89094680721e2c
sh_cvm1 <github@rubyedu.com>
2019-04-17 23:51:06 +0800

    feat(qredis):增加ScanKey/DeleteKey方法

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/go-redis/redis"
+)
+
+func main() {
+	var redisHost string
+	var keyName string
+	var scanCount int64
+	var action string
+	flag.StringVar(&redisHost, "host", "127.0.0.1:6379", "-host 127.0.0.1:6379")
+	flag.StringVar(&keyName, "key", "test", "-key test*")
+	flag.Int64Var(&scanCount, "count", 1000, "-count 1000")
+	flag.StringVar(&action, "action", "scan", "-action scan|delete")
+	flag.Parse()
+
+	redisDB := redis.NewClient(&redis.Options{
+		Addr: redisHost,
+	})
+
+	switch action {
+	case "scan":
+		fmt.Println(*ScanKey(redisHost, keyName, scanCount, redisDB))
+	case "delete":
+		fmt.Println(DeleteKey(ScanKey(redisHost, keyName, scanCount, redisDB), redisDB))
+	}
+}
+
+func ScanKey(redisHost string, keyName string, scanCount int64, redisDB *redis.Client) *[]string {
+	var cursor uint64 = 0
+	var matchKey []string
+	var matchKeyList []string
+
+	for {
+		matchKey, cursor = redisDB.Scan(cursor, keyName, scanCount).Val()
+		matchKeyList = append(matchKeyList, matchKey...)
+		if cursor == 0 {
+			return &matchKeyList
+		}
+	}
+}
+
+func DeleteKey(keyNameList *[]string, redisDB *redis.Client) *redis.IntCmd {
+	return redisDB.Del(*keyNameList...)
+}
